5c81911fed6218db4ca41647b2f108c8880bcdb4
DanaGolub <48007635+DanaGolub@users.noreply.github.com>
2022-01-09 00:28:38 -0800

    create first two objects handling calculation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,25 @@
 import { Component } from '@angular/core';
 
+export class addingNumbers {
+  resultValue!: number;
+  firstNum!: string;
+  secondNum!: string;
+  addition() {
+    this.resultValue = Number(this.firstNum) + Number(this.secondNum)
+  }
+}
+
+export class subtractingNumbers {
+  resultValue!: number;
+  firstNum!: string;
+  secondNum!: string;
+  subtraction() {
+    this.resultValue = Number(this.firstNum) + Number(this.secondNum)
+  }
+
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +27,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mycalculator';
+  sum = 'sum';
+  difference = 'difference';
+  product = 'product';
+  quotient = 'quotient';
+
+  mathematicalOperation: {
+
+  }
 }
