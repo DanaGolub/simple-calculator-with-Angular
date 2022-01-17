9d9061a08b7618aef2b480716b2abea7df27e9f3
DanaGolub <48007635+DanaGolub@users.noreply.github.com>
2022-01-17 09:09:33 -0800

    Add all functionality to this basic calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,5 @@
 import { Component } from '@angular/core';
 
-// export class addingNumbers {
-//   resultValue!: number;
-//   firstNum!: string;
-//   secondNum!: string;
-//   addition() {
-//     this.resultValue = Number(this.firstNum) + Number(this.secondNum)
-//   }
-// }
-
-// export class subtractingNumbers {
-//   resultValue!: number;
-//   firstNum!: string;
-//   secondNum!: string;
-//   subtraction() {
-//     this.resultValue = Number(this.firstNum) - Number(this.secondNum)
-//   }
-// }
-// export class multiplyingNumbers {
-//   resultValue!: number;
-//   firstNum!: string;
-//   secondNum!: string;
-//   multiplication() {
-//     this.resultValue = Number(this.firstNum) * Number(this.secondNum)
-//   }
-// }
-
-// export class dividingNumbers {
-//   resultValue!: number;
-//   firstNum!: string;
-//   secondNum!: string;
-//   division() {
-//     this.resultValue = Number(this.firstNum) / Number(this.secondNum)
-//   }
-// }
-
-// export class performMathOperation {
-//   resultValue!: number;
-//   operator!: string;
-//   firstNum!: string;
-//   secondNum!: string;
-//   choosingOperation() {
-//     if (this.operator === '+'){
-//       this.resultValue = Number(this.firstNum) + Number(this.secondNum)
-//     }
-//     else if (this.operator === '-'){
-//       this.resultValue = Number(this.firstNum) - Number(this.secondNum)
-//     }
-//     else if (this.operator === '*'){
-//       this.resultValue = Number(this.firstNum) * Number(this.secondNum)
-//     }
-//     else if (this.operator === '/'){
-//       this.resultValue = Number(this.firstNum) / Number(this.secondNum)
-//     }
-//     return this.resultValue
-//   }
-// }
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -65,77 +8,84 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'mycalculator';
 
-  // resultValue!: number;
-  // operator!: "+";
-  // firstNum!: string;
-  // secondNum!: string;
-  // doAddition() {
-  //   this.resultValue=Number(this.firstNum) + Number(this.secondNum);
-  // }
-
   resultVal!: number;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  clearningEquations() {
+    this.equationAdd = ""
+    this.equationSubtract = ""
+    this.equationDivide = ""
+    this.equationMultiply = ""
+  }
+
+  //ADDITION//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
   firstNumAdd = "";
   secondNumAdd = "";
   equationAdd = "";
 
   createAddEquation() {
+    this.clearningEquations()
     this.equationAdd = this.firstNumAdd + " + " + this.secondNumAdd + " = " + this.resultVal.toString()
-    console.log(this.equationAdd)
-    // this.equation = 'this.firstNum + " " + this.secondNum + " = " this.result'
+    this.firstNumAdd = "";
+    this.secondNumAdd = "";
   }
   performAddition() {
-    this.resultVal=Number(this.firstNumAdd) + Number(this.secondNumAdd);
-    // console.log(this.resultVal)
+    this.resultVal = Number(this.firstNumAdd) + Number(this.secondNumAdd);
     return this.resultVal
   }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //SUBTRACTION//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
   firstNumSubtract = "";
   secondNumSubtract = "";
   equationSubtract = "";
 
   createSubtractEquation() {
+    this.clearningEquations()
     this.equationSubtract = this.firstNumSubtract + " - " + this.secondNumSubtract + " = " + this.resultVal.toString()
-    console.log(this.equationSubtract)
-    // this.equation = 'this.firstNum + " " + this.secondNum + " = " this.result'
+    this.firstNumSubtract = "";
+    this.secondNumSubtract = "";
   }
   performSubtraction() {
-    this.resultVal=Number(this.firstNumSubtract) - Number(this.secondNumSubtract);
-    // console.log(this.resultVal)
+    this.resultVal = Number(this.firstNumSubtract) - Number(this.secondNumSubtract);
     return this.resultVal
   }
+
+  //MULTIPLICATION//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  firstNumMultiply = "";
+  secondNumMultiply = "";
+  equationMultiply = "";
+
+  createMultiplyEquation() {
+    this.clearningEquations()
+    this.equationMultiply = this.firstNumMultiply + " * " + this.secondNumMultiply + " = " + this.resultVal.toString()
+    this.firstNumMultiply = "";
+    this.secondNumMultiply = "";
+  }
+  performMultiplication() {
+    this.resultVal = Number(this.firstNumMultiply) * Number(this.secondNumMultiply);
+    return this.resultVal
+  }
+
+  //DIVISION//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  firstNumDivide = "";
+  secondNumDivide = "";
+  equationDivide = "";
+
+  createDivideEquation() {
+    this.clearningEquations()
+    this.equationDivide = this.firstNumDivide + " / " + this.secondNumDivide + " = " + this.resultVal.toString()
+    this.firstNumDivide = "";
+    this.secondNumDivide = "";
+  }
+  performDivision() {
+    this.resultVal = Number(this.firstNumDivide) / Number(this.secondNumDivide);
+    return this.resultVal
+  }
+
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  clearEquation() {
-    
-  }
 
+}
 
-
-  }
-
-
-  // sum = 'sum';
-  // difference = 'difference';
-  // product = 'product';
-  // quotient = 'quotient';
-
-  // add: addingNumbers = {
-  //   resultValue: 0,
-  //   firstNum: " ",
-  //   secondNum: " ",
-  //   addition: function (): void {
-  //     throw new Error('Function not implemented.');
-  //   }
-  // }
-
-  // operation: performMathOperation = {
-  //   resultValue: 0,
-  //   firstNum: "",
-  //   secondNum: "",
-  //   operator: "",
-  //   choosingOperation: function (): number {
-  //     throw new Error('Function not implemented.');
-  //   }
-  // }
-// }
